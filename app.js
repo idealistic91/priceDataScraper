3030c0888a68bb5c997d5b9c34471588f8f3739d
idealistic91 <frerichs.jens@hotmail.de>
2019-02-04 18:41:51 +0100

    scraping data from gas station to check if prices have changed

---
+++ app.js
@@ -0,0 +1,97 @@
+var express = require("express"),
+    fs      = require("fs"),
+    request = require("request"),
+    cheerio = require("cheerio");
+    
+var app     = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+var preise = {
+    diesel: "",
+    sorteSuper: "",
+    superPlus: ""
+};
+
+function hoyerRequest (req, res, next){
+
+    request("https://www.hoyer-tankstellen.de/tankstelle/hoyer_autohof_hansator-28217_bremen-hansator_7", function(error, response, html){
+
+        if(!error && response.statusCode == 200){
+            var $ = cheerio.load(html);       
+            console.log("keine fehler");
+            var diesel, sorteSuper, superPlus;
+            
+            
+           
+           $('#sorte_3_1').filter(function(){
+                //checke ob a segment an oder aus ist.
+                var data = $(this);
+                preise.diesel = data.text();
+                console.log(preise.diesel);
+                
+
+            });
+            $('#sorte_5_3').filter(function(){
+                //checke ob a segment an oder aus ist.
+                var data = $(this);
+                preise.sorteSuper = data.text();
+                console.log(preise.sorteSuper);
+                
+                
+
+            });
+            $('#sorte_1_10').filter(function(){
+                //checke ob a segment an oder aus ist.
+                var data = $(this);
+                preise.superPlus = data.text();
+                console.log(preise.superPlus);
+               
+                
+
+            });
+            
+         return preise;   
+            
+        } if(error) {
+            console.log("Fehler: "+error);
+
+         };
+         
+        
+
+    });
+
+    next();
+
+
+};
+
+
+
+ 
+
+ 
+
+app.get("/scrape",hoyerRequest, function(req, res){
+    
+    
+
+    res.render("result", {preise: preise});
+    
+    
+
+    
+
+
+
+
+});
+
+
+app.listen(3000, function(){
+    console.log("started");
+});
+
+// exports = module.exports = app;

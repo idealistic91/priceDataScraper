0deb41be196ca3a65959e7c5c8c1d281b1db03a0
idealistic91 <frerichs.jens@hotmail.de>
2019-02-06 10:04:02 +0100

    bug fix where prices only show when all prices change at once; only show changed values; changed values use red digits now for highlighting

--- app.js
+++ app.js
@@ -22,9 +22,109 @@ var altePreise = {
 
 var alert = {};
 //testfunction -> dummysiteRequest
+function hoyerRequest (req, res, next){
+
+    request("https://www.hoyer-tankstellen.de/tankstelle/hoyer_autohof_hansator-28217_bremen-hansator_7", function(error, response, html){
+
+        if(!error && response.statusCode == 200){
+            var $ = cheerio.load(html);       
+            console.log("Abfrage ");
+            
+            
+           $(".aktualisierungsDatum").filter(function(){
+            var data = $(this);
+            var datum = data.children("strong").text();
+            preise.date = datum;
+            
+           
+
+
+           });
+           
+           $('#sorte_3_1').filter(function(){
+                
+                var data = $(this);
+                if(data.text()!=preise.diesel){
+                    console.log("Diesel has changed");
+
+                    alert = {
+                        oldVal : preise.diesel,
+                        newVal : data.text()
+                    }
+                   
+                    altePreise.diesel = preise.diesel;
+
+                } else {
+                    altePreise.diesel = preise.diesel;
+                };
+                preise.diesel = parseFloat(data.text(), 10);
+                console.log(preise.diesel);
+                
+
+            });
+            $('#sorte_5_3').filter(function(){
+                
+                var data = $(this);
+                if(data.text()!=preise.sorteSuper){
+                    console.log("Super has changed");
+
+                    alert = {
+                        oldVal : preise.sorteSuper,
+                        newVal : data.text()
+                    }
+                    altePreise.sorteSuper = preise.sorteSuper;
+                } else {
+                    altePreise.sorteSuper = preise.sorteSuper;
+                };
+                preise.sorteSuper = parseFloat(data.text(), 10);
+                console.log(preise.sorteSuper);
+                
+                
+
+            });
+            $('#sorte_1_10').filter(function(){
+                
+                var data = $(this);
+                if(data.text()!=preise.superPlus){
+                    console.log("Super plus has changed");
+                    alert = {
+                        oldVal : preise.superPlus,
+                        newVal : data.text()
+                    }
+                    altePreise.superPlus = preise.superPlus;
+
+                } else {
+                    altePreise.superPlus = preise.superPlus;
+                };
+                preise.superPlus = parseFloat(data.text(), 10);
+                console.log(preise.superPlus);
+
+                next();
+                
+
+            });
+            
+            
+            
+        } if(error) {
+            console.log("Fehler: "+error);
+
+         };
+         
+        
+
+    });
+
+    
+
+
+};
+
+
+//MIDDLEWARE
 // function hoyerRequest (req, res, next){
 
-//     request("https://dummysite1337.herokuapp.com", function(error, response, html){
+//     request("https://www.hoyer-tankstellen.de/tankstelle/hoyer_autohof_hansator-28217_bremen-hansator_7", function(error, response, html){
 
 //         if(!error && response.statusCode == 200){
 //             var $ = cheerio.load(html);       
@@ -91,7 +191,7 @@ var alert = {};
 //                 };
 //                 preise.superPlus = parseFloat(data.text(), 10);
 //                 console.log(preise.superPlus);
-//                 next();
+//                    next();
                 
 
 //             });
@@ -111,98 +211,6 @@ var alert = {};
 
 
 // };
-
-
-//MIDDLEWARE
-function hoyerRequest (req, res, next){
-
-    request("https://www.hoyer-tankstellen.de/tankstelle/hoyer_autohof_hansator-28217_bremen-hansator_7", function(error, response, html){
-
-        if(!error && response.statusCode == 200){
-            var $ = cheerio.load(html);       
-            console.log("Abfrage ");
-            
-            
-           $(".aktualisierungsDatum").filter(function(){
-            var data = $(this);
-            var datum = data.children("strong").text();
-            preise.date = datum;
-            
-           
-
-
-           });
-           
-           $('#sorte_3_1').filter(function(){
-                
-                var data = $(this);
-                if(data.text()!=preise.diesel){
-                    console.log("Diesel has changed");
-
-                    alert = {
-                        oldVal : preise.diesel,
-                        newVal : data.text()
-                    }
-                    altePreise.diesel = preise.diesel;
-
-                };
-                preise.diesel = parseFloat(data.text(), 10);
-                console.log(preise.diesel);
-                
-
-            });
-            $('#sorte_5_3').filter(function(){
-                
-                var data = $(this);
-                if(data.text()!=preise.sorteSuper){
-                    console.log("Super has changed");
-
-                    alert = {
-                        oldVal : preise.sorteSuper,
-                        newVal : data.text()
-                    }
-                    altePreise.sorteSuper = preise.sorteSuper;
-                };
-                preise.sorteSuper = parseFloat(data.text(), 10);
-                console.log(preise.sorteSuper);
-                
-                
-
-            });
-            $('#sorte_1_10').filter(function(){
-                
-                var data = $(this);
-                if(data.text()!=preise.superPlus){
-                    console.log("Super plus has changed");
-                    alert = {
-                        oldVal : preise.superPlus,
-                        newVal : data.text()
-                    }
-                    altePreise.superPlus = preise.superPlus;
-
-                };
-                preise.superPlus = parseFloat(data.text(), 10);
-                console.log(preise.superPlus);
-                   next();
-                
-
-            });
-            
-            
-            
-        } if(error) {
-            console.log("Fehler: "+error);
-
-         };
-         
-        
-
-    });
-
-    
-
-
-};
 
 
 //ROUTES
